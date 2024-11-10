5eaaa5a54532a56b35a10d4fd1c4b9d01c76610b
bronsonh <bronson.hill@student.unimelb.edu.au>
2024-11-10 12:32:49 +1100

    Add SQL schema for listings and progress tables

---
+++ tables.sql
@@ -0,0 +1,34 @@
+
+
+CREATE TABLE listings (
+    listing_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    suburb TEXT NOT NULL,
+    postcode TEXT NOT NULL,
+    address TEXT NOT NULL,
+    property_type TEXT NOT NULL,
+    price TEXT,
+    link TEXT,
+    listing_tag TEXT,
+    category TEXT,
+    bathrooms TEXT,
+    parking_spaces TEXT,
+    square_metres TEXT,
+    oldest_scraped_at DATETIME NOT NULL,
+    latest_scraped_at DATETIME NOT NULL,
+    UNIQUE (address, suburb, postcode, property_type)
+);
+
+
+CREATE TABLE progress (
+    progress_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    suburb_name TEXT NOT NULL,
+    postcode TEXT NOT NULL,
+    property_type TEXT NOT NULL,
+    category TEXT NOT NULL,
+    bedrooms INTEGER NOT NULL,
+    page_num INTEGER NOT NULL,
+    scraped_at DATETIME NOT NULL,
+    status TEXT NOT NULL,
+    UNIQUE (suburb_name, postcode, property_type, category, bedrooms)
+);
+
